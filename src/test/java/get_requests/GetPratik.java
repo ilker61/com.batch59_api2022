96c871b913f0c4ee2722183367cbe6153138eed4
ilker61 <jakyuer@gmail.com>
2022-07-12 10:30:48 +0200

    tekrar

--- src/test/java/get_requests/GetPratik.java
+++ src/test/java/get_requests/GetPratik.java
@@ -1,9 +1,11 @@
 package get_requests;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
 
 public class GetPratik {
     /*
@@ -36,14 +38,85 @@ public class GetPratik {
 
     @Test
     public void get1(){
-        String url="https://restful-booker.herokuapp.com/booking/64";
-        Response response=given().when().get(url);
-        response.prettyPrint();
-        response.then().assertThat().statusLine("HTTP/1.1 200 OK").statusCode(200).contentType("application/json");
+
+        // 1. step; set the Url
+
+    String strUrl="https://restful-booker.herokuapp.com/booking/554";
+
+        // 2. step; Set the expected data( POST- PUT- PATCH)
+        // 3. step; Type code to send request
+
+        Response response= given().when().get(strUrl);
+
+        // 4. step; Do assertion
+
+        response.then().assertThat().statusCode(200).contentType(ContentType.JSON).statusLine("HTTP/1.1 200 OK");
+
+
+        // Status code nasil yazdirilir
+
         System.out.println(response.statusCode());
-        System.out.println(response.contentType());
         System.out.println(response.statusLine());
-        System.out.println(response.headers());
-        System.out.println(response.getTime());
+        System.out.println(response.contentType());
+
+        // header kismi nassi yazdirilir loo
+
+        System.out.println(response.header("Connection"));
+        System.out.println(response.header("Host"));
+        System.out.println(response.header("User-Agent"));
+
+        // Tum header ise headers methodu ile yazdirilir
+
+        System.out.println("//////////");
+
+        System.out.println("Tum Headerlar : \n"+response.headers());
+
+        // Time nasil yazdirilibilir loo
+
+        System.out.println("Time Ne LOO: "+response.getTime());
+
+    }
+
+    @Test
+    public void get02(){
+         /*
+        Given
+            https://restful-booker.herokuapp.com/booking/10091
+        When
+            User send a GET Request to the url
+        Then
+            HTTP Status code should be 404
+        And
+            Status Line should be HTTP/1.1 404 Not Found
+        And
+            Response body contains "Not Found"
+        And
+            Response body does not contain "TechProEd"
+        And
+            Server is "Cowboy"
+     */
+        // 1. step; set the url
+
+        String url="https://restful-booker.herokuapp.com/booking/10492";
+
+        // 2. step; set the expected data (Post - Put - Petch)
+        // 3. step; Type code to send request
+
+        Response response=given().when().get(url);
+
+        // 4. step; do assertion
+
+        response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+
+        // body assertionlari yapacagiz
+
+        assertTrue(response.asString().contains("Not Found"));
+        assertFalse(response.asString().contains("TechProEd"));
+
+        // Text deki son gorev ==>  Server is "Cowboy"
+        // bunun icin headers methodunu kullanarak serverin ne oldugunu goruruz
+
+        assertEquals("Cowboy", response.header("Server"));
+
     }
 }
