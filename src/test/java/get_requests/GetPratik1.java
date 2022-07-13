bf1d20b9d5c258c53cba59fe7765009a35bed625
ilker61 <jakyuer@gmail.com>
2022-07-13 10:16:04 +0200

    get07 calistik

--- src/test/java/get_requests/GetPratik1.java
+++ src/test/java/get_requests/GetPratik1.java
@@ -203,4 +203,38 @@ public class GetPratik1 extends JsonPlaceHolderBaseUrl {
 
     }
 
+    @Test
+    public void get07(){
+
+        /*
+        Given
+	   	    https://jsonplaceholder.typicode.com/todos
+		When
+			 I send GET Request to the URL
+		Then
+			 1)Status code is 200
+			 2)Print all ids greater than 190 on the console
+			   Assert that there are 10 ids greater than 190
+			 3)Print all userIds whose ids are less than 5 on the console
+			   Assert that the number of userIds whose ids are less than 5 is 4
+			 4)Print all titles whose ids are less than 5
+			   Assert that "delectus aut autem" is one of the titles whose id is less than 5
+     */
+
+        // 1. step; set the Url
+
+        spec.pathParam("first", "todos");
+
+        // 2. step; set the expected data
+        // 3. step; send the request get the response
+
+        Response response=given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+        // 4. step; do assertions
+
+
+
+    }
+
 }
