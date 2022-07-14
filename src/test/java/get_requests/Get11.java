e839e91df761d1aa3916f24ae21e4779194c6f3c
ilker61 <jakyuer@gmail.com>
2022-07-14 22:01:30 +0200

    get11()

--- src/test/java/get_requests/Get11.java
+++ src/test/java/get_requests/Get11.java
@@ -1,12 +1,15 @@
 package get_requests;
 
 import base_urls.GoRestBaseUrl;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertTrue;
 
 public class Get11 extends GoRestBaseUrl {
 
@@ -24,9 +27,9 @@ public class Get11 extends GoRestBaseUrl {
         And
             We have at least one "active" status
         And
-            "Indra Ganaka", "Sarada Mehrotra", "Jagathi Chopra" are among the users
+            "Aalok Acharya DDS", "Agastya Somayaji", "Acharyasuta Chattopadhyay DC" are among the users
         And
-            The female users are more than male users
+            The female users are more than male users or equals
      */
 
     @Test
@@ -44,18 +47,44 @@ public class Get11 extends GoRestBaseUrl {
         response.prettyPrint();
 
         // 4. Step; Do Assertion
-        // 1. yol; body methodu ile
+
 
         response.
                 then().
                 assertThat().
                 statusCode(200).
-                body("meta.pagination.limit", equalTo(10),
+                body("meta.pagination.limit",equalTo(10),
                         "meta.pagination.links.current", equalTo("https://gorest.co.in/public/v1/users?page=1"),
-                        "data.id", hasSize(10));
+                        "data.id", hasSize(10),
+                        "data.status", hasItem("active"),
+                        "data.name", hasItems("Aalok Acharya DDS", "Acharyasuta Chattopadhyay DC", "Shresth Nehru"));
+
+
+        //Bayan ve erkek sayısını karşılaştıralım
+        //1. Yol: Tüm cinisyetli çekip bayan sayısı ile karşılaştıralım.
+        JsonPath json = response.jsonPath();
+        List<String> genders = json.getList("data.gender");
+        System.out.println(genders);
+
+        int numOfFemales=0;
+        for(String w: genders){
+            if(w.equalsIgnoreCase("female")){
+                numOfFemales++;
+            }
+        }
+        System.out.println(numOfFemales);//6
+        assertTrue(numOfFemales > genders.size()-numOfFemales);
+
+        //2. Yol:  Tüm bayan ve bayları ayrı ayrı Groovy ile çekelim.
+        List<String> femaleList = json.getList("data.findAll{it.gender=='female'}.gender");
+        System.out.println(femaleList);
+
+        List<String> maleList = json.getList("data.findAll{it.gender=='male'}.gender");
+        System.out.println(maleList);
+
+        assertTrue(femaleList.size()>maleList.size());
+
 
 
     }
-
-
 }
