5e2562424f5c0456f8817def1857bf9277347ec5
ilker61 <jakyuer@gmail.com>
2022-07-18 12:12:21 +0200

    patchPratik01()

--- src/test/java/patch_request/PatchPratik1.java
+++ src/test/java/patch_request/PatchPratik1.java
@@ -1,4 +1,30 @@
 package patch_request;
 
-public class PatchPratik1 {
+import base_urls.JsonPlaceHolderBaseUrl;
+import org.junit.Test;
+
+public class PatchPratik1 extends JsonPlaceHolderBaseUrl {
+
+    /*
+        Given
+	        1) https://jsonplaceholder.typicode.com/todos/198
+	        2) {
+                 "title": "Wash the dishes"
+               }
+        When
+	 		I send PATCH Request to the Url
+	    Then
+	   	   Status code is 200
+	   	   And response body is like   {
+									    "userId": 10,
+									    "title": "Wash the dishes",
+									    "completed": true,
+									    "id": 198
+									   }
+     */
+
+    @Test
+    public void patch01(){
+
+    }
 }
