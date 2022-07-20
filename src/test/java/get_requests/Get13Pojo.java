74b9b53ee2836e232d01f8424c06fbaa4ce8fdd2
ilker61 <jakyuer@gmail.com>
2022-07-20 10:36:46 +0200

    19.07.22 pojo class

---
+++ src/test/java/get_requests/Get13Pojo.java
@@ -0,0 +1,24 @@
+package get_requests;
+
+public class Get13Pojo {
+    /*
+    Given
+            https://gorest.co.in/public/v1/users/2334
+        When
+            User send GET Request to the URL
+        Then
+            Status Code should be 200
+        And
+            Response body should be like
+            {
+    "meta": null,
+    "data": {
+        "id": 2334,
+        "name": "Aaditya Mehrotra CPA",
+        "email": "aaditya_mehrotra_cpa@flatley-bernhard.co",
+        "gender": "male",
+        "status": "inactive"
+    }
+}
+     */
+}
