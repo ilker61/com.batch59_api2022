43b4f823b550dc73660438f695e2533a1c12495e
ilker61 <jakyuer@gmail.com>
2022-07-21 09:44:20 +0200

    21.07.22 pratik

--- src/test/java/get_requests/GetPratik2.java
+++ src/test/java/get_requests/GetPratik2.java
@@ -4,6 +4,9 @@ import base_urls.HerOkuAppBaseUrl;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.Test;
+import utils.JsonUtil;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.*;
 
@@ -58,16 +61,16 @@ public class GetPratik2 extends HerOkuAppBaseUrl {
         And
             Response body should be like;
           {
-            "firstname": "Andrew",
-            "lastname": "Anderson",
-            "totalprice": 163,
-            "depositpaid": false,
-            "bookingdates": {
-                "checkin": "2022-10-03",
-                "checkout": "2022-10-11"
-            },
-            "additionalneeds": "lunch"
-        }
+    "firstname": "c8997bba-b644-499b-b37c-698018b8ad9c",
+    "lastname": "415ecc27-abc6-43f2-9b91-626daaf70bdd",
+    "totalprice": 121,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2022-07-23",
+        "checkout": "2022-07-26"
+    },
+    "additionalneeds": "lunch"
+}
      */
 
         // 1. step; set the Url
@@ -86,9 +89,58 @@ public class GetPratik2 extends HerOkuAppBaseUrl {
         response.then().assertThat().
                 statusCode(200).
                 contentType(ContentType.JSON).
-                body("firstname",equalTo("Andrew") ,"lastname", equalTo("Anderson"),
-                        "totalprice", equalTo(163),"depositpaid", equalTo(false),
-                        "bookingdates.checkin" , equalTo("2022-10-03"),
-                        "bookingdates.checkout", equalTo("2022-10-11"),"additionalneeds", equalTo("lunch") );
+                body("firstname",equalTo("c8997bba-b644-499b-b37c-698018b8ad9c") ,"lastname", equalTo("415ecc27-abc6-43f2-9b91-626daaf70bdd"),
+                        "totalprice", equalTo(121),"depositpaid", equalTo(true),
+                        "bookingdates.checkin" , equalTo("2022-07-23"),
+                        "bookingdates.checkout", equalTo("2022-07-26"),"additionalneeds", equalTo("lunch") );
+    }
+
+    // get06() icin 2. yol
+
+    @Test
+    public void get06Mapper(){
+
+        // 1. step; set the Url
+
+        spec.pathParams("first", "booking", "second", 101);
+
+        // 2. step; set the expected data
+
+        String expectedData="{\n" +
+                "    \"firstname\": \"c8997bba-b644-499b-b37c-698018b8ad9c\",\n" +
+                "    \"lastname\": \"415ecc27-abc6-43f2-9b91-626daaf70bdd\",\n" +
+                "    \"totalprice\": 121,\n" +
+                "    \"depositpaid\": true,\n" +
+                "    \"bookingdates\": {\n" +
+                "        \"checkin\": \"2022-07-23\",\n" +
+                "        \"checkout\": \"2022-07-26\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\": \"lunch\"\n" +
+                "}";
+
+        HashMap<String, Object> expectedDataMap = JsonUtil.convertJsonToJavaObject(expectedData, HashMap.class);
+
+        // 3. step; send the get request and get the response
+
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+        // 4. step; do assertion
+
+       HashMap<String, Object> actualDataMap = JsonUtil.convertJsonToJavaObject(response.asString(), HashMap.class);
+
+        assertEquals(200, response.getStatusCode());
+
+        assertEquals(expectedDataMap.get("firstname"), actualDataMap.get("firstname"));
+        assertEquals(expectedDataMap.get("lastname"), actualDataMap.get("lastname"));
+        assertEquals(expectedDataMap.get("totalprice"), actualDataMap.get("totalprice"));
+        assertEquals(expectedDataMap.get("depositpaid"), actualDataMap.get("depositpaid"));
+
+        assertEquals(expectedDataMap.get("checkin"), actualDataMap.get("checkin"));
+
+
+
+
+
+
     }
 }
