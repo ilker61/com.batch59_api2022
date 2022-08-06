197a4be12621aa24f2f0727bc829cebaab2794fe
ilker61 <jakyuer@gmail.com>
2022-08-06 17:30:35 +0200

    06.08.22 room pojo

---
+++ src/test/java/pojos/RoomPost.java
@@ -0,0 +1,94 @@
+package pojos;
+
+public class RoomPost {
+
+    private String createdBy;
+    private String createdDate;
+    private String description;
+    private Integer price;
+    private Integer roomNumber;
+    private String roomType;
+    private Boolean status;
+
+    public RoomPost(String createdBy, String createdDate, String description, Integer price, Integer roomNumber, String roomType, Boolean status) {
+        this.createdBy = createdBy;
+        this.createdDate = createdDate;
+        this.description = description;
+        this.price = price;
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.status = status;
+    }
+
+    public RoomPost() {
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomPost{" +
+                "createdBy='" + createdBy + '\'' +
+                ", createdDate='" + createdDate + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", roomNumber=" + roomNumber +
+                ", roomType='" + roomType + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
